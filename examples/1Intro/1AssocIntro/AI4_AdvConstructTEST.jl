c2eeb31552b817743b88c85987e010b16b81ab7d
Lauren Milechin <lauren.milechin@mit.edu>
2017-10-02 15:16:49 -0400

    comment out lines that dont work for now

--- examples/1Intro/1AssocIntro/AI4_AdvConstructTEST.jl
+++ examples/1Intro/1AssocIntro/AI4_AdvConstructTEST.jl
@@ -14,10 +14,10 @@ A05 = Assoc("","",[]);           # All empty.
 A06 = Assoc("",iNum,[]);         # Row and value empty.
 
 # Mixed type non-empty arrays.
-A11 = Assoc("a,",iStr,iNum);     # String scalar,  string vector,  numeric vector.
-A12 = Assoc(iNum,iStr,"a,");     # Numeric vector, string vector,  string scalar.
-A13 = Assoc(iNum,1,"a,");        # Numeric vector, numeric scalar, string scalar.
+#A11 = Assoc("a,",iStr,iNum);     # String scalar,  string vector,  numeric vector.
+#A12 = Assoc(iNum,iStr,"a,");     # Numeric vector, string vector,  string scalar.
+#A13 = Assoc(iNum,1,"a,");        # Numeric vector, numeric scalar, string scalar.
 A14 = Assoc("a,",1,1);           # String scalar,  numeric scalar, numeric scalar.
 A15 = Assoc("a,","a,",1);        # String scalar,  string scalar,  numeric vector.
-A16 = Assoc("a,",iNum,1);        # String scalar,  numeric vector, numeric scalar.
+#A16 = Assoc("a,",iNum,1);        # String scalar,  numeric vector, numeric scalar.
 
