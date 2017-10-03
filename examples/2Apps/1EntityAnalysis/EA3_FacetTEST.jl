17d45311b7824c0e128987bbc9a9c3012a82097e
Lauren Milechin <lauren.milechin@mit.edu>
2017-10-03 13:02:18 -0400

    compatibility fix

--- examples/2Apps/1EntityAnalysis/EA3_FacetTEST.jl
+++ examples/2Apps/1EntityAnalysis/EA3_FacetTEST.jl
@@ -8,7 +8,7 @@ E = logical(E)
 
 x = "LOCATION/new york,";
 p = "PERSON/michael chang,";
-F = ( nocol(E[:,x]) & nocol(E[:,p]))' * E;
+F = ( nocol(E[:,x]) .& nocol(E[:,p]))' * E;
 #print(F' > 1 )
 F' > 1
 
