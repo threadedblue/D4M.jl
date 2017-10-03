9778403df3406ccc7f8599b20d47bf9a26944071
Lauren Milechin <lauren.milechin@mit.edu>
2017-10-03 10:25:22 -0400

    temporary fix

--- examples/2Apps/1EntityAnalysis/EA3_FacetTEST.jl
+++ examples/2Apps/1EntityAnalysis/EA3_FacetTEST.jl
@@ -9,8 +9,9 @@ E = logical(E)
 x = "LOCATION/new york,";
 p = "PERSON/michael chang,";
 F = ( nocol(E[:,x]) & nocol(E[:,p]))' * E;
-print(F' > 1 )
+#print(F' > 1 )
+F’ > 1
 
 Fn = F./ sum(E,1)
-print((Fn' > 0.02))
-
+#print((Fn' > 0.02))
+Fn' > 0.02
