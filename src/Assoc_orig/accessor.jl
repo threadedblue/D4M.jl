84f9931e79d3ddb9e8a082b87ab92865603d0477
Lauren Milechin <lauren.milechin@mit.edu>
2017-12-04 12:35:31 -0500

    fix of Row and Col for numeric row/col

--- src/Assoc_orig/accessor.jl
+++ src/Assoc_orig/accessor.jl
@@ -7,16 +7,14 @@ function Adj(A::Assoc)
 end
 
 function Col(A::Assoc)
-    if isa(A.col[1], AbstractString)
+    if isa(A.col[1], AbstractString) || ~(size(A.A,2) == A.col[end])
         return copy(A.col)
     end
     return 1:size(A.A,2)
 end
 
-
-
 function Row(A::Assoc)
-    if isa(A.row[1], AbstractString)
+    if isa(A.row[1], AbstractString) || ~(size(A.A,1) == A.row[end])
         return copy(A.row)
     end
     return 1:size(A.A,1)
