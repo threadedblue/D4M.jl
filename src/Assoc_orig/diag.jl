c985df645ba78b58ea12987de3f3d4dfdebf8d16
Lauren Milechin <lauren.milechin@mit.edu>
2017-12-06 12:00:48 -0500

    Handle diag case where vals are in the Adj

--- src/Assoc_orig/diag.jl
+++ src/Assoc_orig/diag.jl
@@ -4,7 +4,12 @@ diag : Output the diagonal of input Assoc A.
 Outputs the Assoc with only the diagonal elements of A.
 =#
 function diag(A::Assoc)
-    return deepCondense(Assoc(A.row,A.col,A.val,sparse(diagm(diag(A.A)))))
+    # Check if numeric values first
+    if A.val == [1.0]
+        return Assoc(A.row,A.col,A.val,dropzeros!(sparse(diagm(diag(A.A)))))
+    else
+        return deepCondense(Assoc(A.row,A.col,A.val,sparse(diagm(diag(A.A)))))
+    end
 end
 
 
