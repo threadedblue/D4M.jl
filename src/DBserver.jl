efea08f6f26ac914987324f92d55ea45bb6bff28
Lauren Milechin <Lauren@Guest2s-MacBook.local>
2018-02-08 11:52:02 -0500

    adding preliminary DB ops

---
+++ src/DBserver.jl
@@ -0,0 +1,94 @@
+
+# A DBserver struct contains the connect information for a database.
+struct DBserver
+    instanceName::String
+    host::String
+    user::String
+    pass::String
+    dbType::String
+end
+
+# dbsetup calls the DBserver constructor given configuration information.
+# When provided only the instance name, dbsetup assumes the default configuration directory.
+#   The default location is that on the MIT Supercloud.
+#   Assumes existance of two files: accumulo_user_password.txt and dnsname containing password and hostname, respectively.
+#   Assumes username is AccumuloUser.
+# When provided a directory, will assume configuration files are in that directory.
+#   Assumes existance of two files: accumulo_user_password.txt and dnsname containing password and hostname, respectively.
+#   Assumes username is AccumuloUser.
+# When provided a configuration file, will read in and use the configuration in the file.
+#   See example configuration file for formatting.
+function dbsetup(instance, config="/home/gridsan/tools/groups/")
+    # Note default value for config location is the MIT Supercloud default location
+    if isdir(config) # Config dir
+        dbdir = config*"/databases/"*instance
+        f = open(dbdir*"/accumulo_user_password.txt","r")
+        pword = readstring(f)
+        f = open(dbdir*"/dnsname","r")
+        hostname = replace(readstring(f),"\n","")*":2181"
+        DB = DBserver(instance,hostname,"AccumuloUser",pword,"BigTableLike")
+    else # Conifg file
+        f = open(config)
+        conf = readlines(config)
+        conf = Dict(l[1] => l[2] for l in split.(conf,"="))
+
+        DB = DBserver(conf["instance"],conf["hostname"],conf["username"],conf["password"],"BigTableLike")
+    end
+    return DB
+end
+
+using JavaCall
+# ls returns a list of tables that exist in the DBserver DB.
+function ls(DB::DBserver)
+    dbInfo = @jimport "edu.mit.ll.d4m.db.cloud.D4mDbInfo"
+    dbInfoObj = dbInfo((JString, JString, JString, JString,), DB.instanceName, DB.host, DB.user, DB.pass)
+    
+    tables = jcall(dbInfoObj, "getTableList", JString, (),)
+    
+    return split(tables[1:end-1],tables[end])
+end
+
+# getindex returns a binding to a table. If the table does it exist, it creates the table.
+# A database table struct is returned.
+function getindex(DB::DBserver,tableName::String)
+    
+    ops = @jimport "edu.mit.ll.d4m.db.cloud.D4mDbTableOperations"
+    opsObj = ops((JString, JString, JString, JString,), DB.instanceName, DB.host, DB.user, DB.pass)
+    
+    if ~any(ls(DB) .== tableName) # Create new table if it doesn't exist
+        println("Creating "*tableName*" in "*DB.instanceName);
+        jcall(opsObj,"createTable", Void, (JString,), tableName)
+    end
+    
+    d4mQuery = @jimport "edu.mit.ll.d4m.db.cloud.D4mDataSearch"
+    queryObj = d4mQuery((JString, JString, JString, JString, JString,), DB.instanceName, DB.host, tableName, DB.user, DB.pass)
+    
+    return DBtable(DB, tableName, "", 0, 0, "", 5e5, queryObj,opsObj)
+    
+end
+
+# getindex returns a binding to a table. If the table does it exist, it creates the table.
+# When a second table name is provided, a database table pair is returned.
+function getindex(DB::DBserver,tableName1::String,tableName2::String)
+    
+    ops = @jimport "edu.mit.ll.d4m.db.cloud.D4mDbTableOperations"
+    opsObj = ops((JString, JString, JString, JString,), DB.instanceName, DB.host, DB.user, DB.pass)
+    
+    # Create new tables if they don't exist
+    if ~any(ls(DB) .== tableName1) || ~any(ls(DB) .== tableName2)
+        if ~any(ls(DB) .== tableName1)
+            println("Creating "*tableName1*" in "*DB.instanceName);
+            jcall(opsObj,"createTable", Void, (JString,), tableName1)
+        end
+        if ~any(ls(DB) .== tableName2)
+            println("Creating "*tableName2*" in "*DB.instanceName);
+            jcall(opsObj,"createTable", Void, (JString,), tableName2)
+        end
+    end
+    
+    d4mQuery = @jimport "edu.mit.ll.d4m.db.cloud.D4mDataSearch"
+    queryObj = d4mQuery((JString, JString, JString, JString, JString,), DB.instanceName, DB.host, tableName1, DB.user, DB.pass)
+    
+    return DBtablePair(DB, tableName1, tableName2, "", 0, 0, "", 5e5, queryObj,opsObj)
+    
+end
---
+++ src/DBtable.jl
@@ -0,0 +1,204 @@
+
+# DBtable contains the table binding information, as well as the
+# d4mQuery Java object for the table.
+struct DBtable
+    DB::DBserver
+    name::String
+    security::String
+    numLimit::Int
+    numRow::Int
+    columnfamily::String
+    putBytes::Number
+    d4mQuery
+    tableOps
+end
+
+# DBtablePair contains the table pair binding information, as well as the
+# d4mQuery Java object for the table.
+# The DBtablePair contains bindings to both a table and its transpose.
+struct DBtablePair
+    DB::DBserver
+    name1::String
+    name2::String
+    security::String
+    numLimit::Int
+    numRow::Int
+    columnfamily::String
+    putBytes::Number
+    d4mQuery
+    tableOps
+end
+
+# Deletes specified DB table
+# TODO: add check whether want to delete
+function delete(table::DBtable)
+    jcall(table.tableOps,"deleteTable", Void, (JString,), table.name)
+ end
+ 
+ function delete(table::DBtablePair)
+     jcall(table.tableOps,"deleteTable", Void, (JString,), table.name1)
+     jcall(table.tableOps,"deleteTable", Void, (JString,), table.name2)
+ end
+
+ # Helper function to convert Assoc fields to an Accumulo-friendly string
+ function toDBstring(input)
+    StringArray = Array{T} where T <: AbstractString
+    NumericArray = Array{T} where T <: Number
+    UnionArray = Array{T} where T <: Union{AbstractString,Number}
+    
+    if isa(input,StringArray)
+        output = join(input,"\n")*"\n"
+    elseif isa(input,NumericArray)
+        output = string.(input)
+    elseif isa(input,UnionArray)
+        output = convert(Array{AbstractString},input)
+    elseif isa(input,Colon)
+        output = ":"
+    elseif isa(input,StartsWith)
+        str = S.inputString
+        output = ""
+        del = str[end]
+        idx1 = 1
+        idx2 = search(str,del,idx1+1)
+
+        while idx2 > 0
+            output = output*str[idx1:idx2]*":"*del*str[idx1:idx2-1]*Char(127)*del
+            idx1 = idx2+1
+            idx2 = search(str,del,idx2+1)
+        end
+    else
+        output = input
+    end
+    
+    return output
+end
+
+# Base getindex function- i and j are d4m formatted strings (delimitered)
+DBtableType = Union{DBtable,DBtablePair}
+function getindex(table::DBtableType,i::AbstractString,j::AbstractString)
+    
+    jcall(table.d4mQuery,"setCloudType", Void, (JString,), table.DB.dbType)
+    jcall(table.d4mQuery,"setLimit", Void, (jint,), table.numLimit)
+    jcall(table.d4mQuery,"reset", Void, (),)
+    
+    dbResultSet = @jimport "edu.mit.ll.d4m.db.cloud.D4mDbResultSet"
+    
+    if i!=":" || j==":" || isa(table,DBtable)
+        
+        if isa(table,DBtablePair)
+            jcall(table.d4mQuery,"setTableName", Void, (JString,), table.name1)
+        end
+        jcall(table.d4mQuery,"doMatlabQuery",dbResultSet,(JString,JString,JString,JString,),i,j,table.columnfamily,table.security)
+
+        r = jcall(table.d4mQuery,"getRowReturnString", JString, (),)
+        c = jcall(table.d4mQuery,"getColumnReturnString", JString, (),)
+        
+    else # Search transpose table if column query
+        jcall(table.d4mQuery,"setTableName", Void, (JString,), table.name2)
+        jcall(table.d4mQuery,"doMatlabQuery",dbResultSet,(JString,JString,JString,JString,),j,i,table.columnfamily,table.security)
+
+        c = jcall(table.d4mQuery,"getRowReturnString", JString, (),)
+        r = jcall(table.d4mQuery,"getColumnReturnString", JString, (),)
+    end
+    
+    v = jcall(table.d4mQuery,"getValueReturnString", JString, (),)
+    
+    return deepCondense(Assoc(r,c,v))
+    
+end
+
+UnionArray = Array{T} where T <: Union{AbstractString,Number}
+ValidQueryTypes = Union{Colon,AbstractString,UnionArray,StartsWith}
+
+getindex(table::DBtableType,i::ValidQueryTypes,j::ValidQueryTypes) = getindex(table,toDBstring(i),toDBstring(j))
+
+function searchall(str::String,c::Char)
+    idx = search(str,c)
+    allIdx = idx
+
+    while idx < endof(str)
+        idx = search(str,c,idx+1)
+        allIdx = [allIdx idx]
+    end
+
+    return allIdx
+end
+
+# putTriple is the main db ingest function
+DBtableType = Union{DBtable,DBtablePair}
+function putTriple(table::DBtableType,r::AbstractString,c::AbstractString,v::AbstractString)
+    
+    # Find chunk size for ingest
+    chunkBytes = table.putBytes
+    rIdx = [0 searchall(r,r[end])]
+    cIdx = [0 searchall(c,c[end])]
+    vIdx = [0 searchall(v,v[end])]
+    numTriples = NumStr(r);
+    avgBytePerTriple = (length(r) + length(c) + length(v))/numTriples;
+    chunkSize = min(max(1,round(Integer,chunkBytes/avgBytePerTriple)),numTriples);
+    
+    # In Matlab D4M this is inside the loop- do we need to make a new object each time?
+    dbInsert = @jimport "edu.mit.ll.d4m.db.cloud.D4mDbInsert"
+    
+    if isa(table,DBtablePair)
+        insertObj = dbInsert((JString, JString, JString, JString, JString,), table.DB.instanceName, table.DB.host, table.name1, table.DB.user, table.DB.pass)
+        insertObjT = dbInsert((JString, JString, JString, JString, JString,), table.DB.instanceName, table.DB.host, table.name2, table.DB.user, table.DB.pass)
+    else
+        insertObj = dbInsert((JString, JString, JString, JString, JString,), table.DB.instanceName, table.DB.host, table.name, table.DB.user, table.DB.pass)
+    end
+        
+    # Insert each chunk
+    for i in 1:chunkSize:numTriples
+        iNext = min(i + chunkSize,numTriples+1);
+        rr = r[(rIdx[i]+1):rIdx[iNext]];
+        cc = c[(cIdx[i]+1):cIdx[iNext]];
+        vv = v[(vIdx[i]+1):vIdx[iNext]];
+        
+        jcall(insertObj,"doProcessing",Void,(JString,JString,JString,JString,JString,),rr, cc, vv, table.columnfamily, table.security)
+        
+        # Insert transpose into transpose table if DBtablePair
+        if isa(table,DBtablePair)
+            jcall(insertObjT,"doProcessing",Void,(JString,JString,JString,JString,JString,),cc, rr, vv, table.columnfamily, table.security)
+        end
+    end
+    
+end
+
+UnionArray = Array{T} where T <: Union{AbstractString,Number}
+ValidType = Union{UnionArray,AbstractString}
+
+putTriple(table::DBtableType,r::ValidType,c::ValidType,v::ValidType) = putTriple(table,toDBstring(r),toDBstring(c),toDBstring(v))
+
+# The put function deconstructs A and calls putTriple
+function put(table::DBtableType,A::Assoc)
+    r,c,v = find(A)
+    putTriple(table,r,c,v)
+end
+
+#addColCombiner: Adds combiners to specific column names.
+# Inputs: T = database table object
+    # colNames = string list of column names. If ':,' (default) then sets all columns to combining.
+    # combineType = combiner name "min", "max", or "sum";
+    #      IF D4M_API_JAVA.jar is installed on the Accumulo instance, you can
+    #      specify "min_decimal", "max_decimal", or "sum_decimal"
+    #      to obtain the ability to combine on decimals
+function addColCombiner(table::DBtable,colNames=":,",combineType="sum")   
+        jcall(table.tableOps,"designateCombiningColumns", Void, (JString, JString, JString, JString,), table.name, colNames, combineType, table.columnfamily)
+end
+
+# nnz returns the number of entries in specified table
+function nnz(table::DBtableType)
+    if isa(table,DBtablePair)
+        tname = table.name1
+    else
+        tname = table.name
+    end
+    
+    arrayList = @jimport "java.util.ArrayList"
+    list = @jimport "java.util.List"
+    
+    tableList = arrayList((),)
+    jcall(tableList,"add",jboolean,(JObject,),Tadj.name1)
+    
+    jcall(table.tableOps,"getNumberOfEntries",jlong,(list,),tableList)
+end
