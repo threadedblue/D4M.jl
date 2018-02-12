7630f0fb776fa3ab0116f204354d0c9738447474
Lauren Milechin <lauren.milechin@mit.edu>
2018-02-12 12:29:08 -0500

    calculate degree distribution on Assocs

--- src/GraphDegree.jl
+++ src/GraphDegree.jl
@@ -4,6 +4,9 @@ InDegree : Calculate the in-degree distribution of graph A
 =#
 
 function OutDegree(A)
+    if isa(A,Assoc)
+        A = A.A
+    end
     dout = sum(A,2)
     dout_i,dout_j,dout_v = findnz(dout)
     ndout = sum(sparse(dout_i,dout_v,1),1)
@@ -11,10 +14,13 @@ function OutDegree(A)
 end
 
 function InDegree(A)
+    if isa(A,Assoc)
+        A = A.A
+    end
     din = sum(A,1)
     din_i,din_j,din_v = findnz(din)
     ndin = sum(sparse(din_i,din_v,1),1)
-    return nin
+    return ndin
 end
 
 ########################################################
