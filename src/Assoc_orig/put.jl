6b35b97c25fe961a33b25ba61533c5bf4b9123c0
Lauren Milechin <lauren.milechin@mit.edu>
2018-06-06 12:21:18 -0400

    add put (Row,Col,Val) for scalar case

--- src/Assoc_orig/put.jl
+++ src/Assoc_orig/put.jl
@@ -1,11 +1,26 @@
 putAdj(A::Assoc,AA::AbstractSparseMatrix) =  Assoc(copy(A.row),copy(A.col),copy(A.val),AA)
 putAdj(A::Assoc,AA::Array{Int,2}) =  putAdj(A::Assoc,sparse(AA))
 
-putRow(A::Assoc,ARow::Array{Union{AbstractString,Number},1}) =  Assoc(ARow,copy(A.col),copy(A.val),copy(A.A))
+StringOrNum = Union{AbstractString,Number}
+UnionArray = Array{Union{AbstractString,Number}}
 
-putCol(A::Assoc,ACol::Array{Union{AbstractString,Number},1}) =  Assoc(copy(A.row),ACol,copy(A.val),copy(A.A))
+# put for new array
+putRow(A::Assoc,ARow::UnionArray) =  Assoc(ARow,copy(A.col),copy(A.val),copy(A.A))
+putCol(A::Assoc,ACol::UnionArray) =  Assoc(copy(A.row),ACol,copy(A.val),copy(A.A))
+putVal(A::Assoc,AVal::UnionArray) =  Assoc(copy(A.row),copy(A.col),AVal,copy(A.A))
 
-putVal(A::Assoc,AVal::Array{Union{AbstractString,Number},1}) =  Assoc(copy(A.row),copy(A.col),AVal,copy(A.A))
+# put for new scalar 
+function putRow(A::Assoc,ARow::StringOrNum)
+    r,c,v = find(A)
+    Assoc(ARow,c,v)
+end
 
+function putCol(A::Assoc,ACol::StringOrNum)
+    r,c,v = find(A)
+    Assoc(r,ACol,v)
+end
 
-
+function putVal(A::Assoc,AVal::StringOrNum)
+    r,c,v = find(A)
+    Assoc(r,c,AVal)
+end
