b1a635ac4701a2e43da8b75b1573f68bc989414c
Lauren Milechin <lauren.milechin@mit.edu>
2018-10-11 13:03:38 -0400

    formatting output of example

--- examples/1Intro/1AssocIntro/AI2_SubsrefTEST.jl
+++ examples/1Intro/1AssocIntro/AI2_SubsrefTEST.jl
@@ -3,36 +3,42 @@ println("This is a test on the subreferencing of Associative Array")
 A = ReadCSV("A.csv")
 
 # Get rows a and b.
+println("Get rows a and b:")
 A1r = A["a,b,",:];
-print(A1r)
+printFull(A1r)
 
 # Get rows containing a and columns 1 thru 3.
 #TODO A2r = A("a *,",1:3)
 # print(A2r)
 
 # Get rows a to b.
+println("Get rows a through b:")
 A3r = A["a,:,b,",:]
-print(A3r)
+printFull(A3r)
 
 # Get rows starting with a or c.
+println("Get rows starting with a or c:")
 A4r = A[StartsWith("a,c,"),:]
-print(A4r)
+printFull(A4r)
 
 # Get cols a and b.
+println("Get cols a and b:")
 A1c = A[:,"a,b,"]
-print(A1c)
+printFull(A1c)
 
 # Get rows 1 thru 3 and cols containing a.
 #TODO A2c = A(1:3,"a *,")
 #print(A2c)
 
 # Get cols a to b.
+println("Get cols a through b:")
 A3c = A[:,"a,:,b,"]
-print(A3c)
+printFull(A3c)
 
-# Get cols starting with a or b.
+# Get cols starting with a or c.
+println("Get cols starting with a or c:")
 A4c = A[:,StartsWith("a,c,")]
-print(A4c)
+printFull(A4c)
 
 # Get all values less than b.
 #TODO A1v = (A < "b,")
