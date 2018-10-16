eb2f8cc919ee37c90678f55fcaa48dc03f820590
Lauren Milechin <lauren.milechin@mit.edu>
2018-10-16 16:41:48 -0400

    final tablemult fix (hopefully)

--- src/Graphulo/tablemult.jl
+++ src/Graphulo/tablemult.jl
@@ -2,7 +2,6 @@ using JavaCall
 #=
 tablemult : matrix multiply between two Accumulo tables.
 =#
-DBtableType = Union{DBtable,DBtablePair}
 
 #= TableMult
 Performs matrix multiply A * B of tables A and B
@@ -14,13 +13,14 @@ Inputs:
 Optional Input:
     CT: String indicating the name of transpose of result table, default empty string does not create transpose table
 Key/Value Inputs:
-    rowfilter: string indicating which rows of AT and B to perform the multiply on, formatted like D4M query strings
+    rowfilter: string, indicating which rows of AT and B to perform the multiply on, formatted like D4M query strings
     colfilterAT: string indicating which columns of AT to perform the multiply on, formatted like D4M query strings
     colfilterB: string indicating which columns of B to perform the multiply on, formatted like D4M query strings
+    (rowfilter, colfilterAT, and colfilterB can be any valid query type, including a string array or StartsWith)
 Ouptut:
 Returns a binding to the new table (or table pair if transpose table is specified)
 =#
-function tablemult(AT::DBtableType,B::DBtableType,C::AbstractString,CT::AbstractString=""; rowfilter::AbstractString="",colfilterAT::AbstractString="",colfilterB::AbstractString="")
+function tablemult(AT::DBtableType,B::DBtableType,C::AbstractString,CT::AbstractString=""; rowfilter::ValidQueryTypes="",colfilterAT::ValidQueryTypes="",colfilterB::ValidQueryTypes="")
 
     if isa(AT,DBtablePair)
         ATname = AT.name1
@@ -36,7 +36,7 @@ function tablemult(AT::DBtableType,B::DBtableType,C::AbstractString,CT::Abstract
 
     jcall(AT.DB.Graphulo,"TableMult", jlong, 
         (JString,JString,JString,JString,JString,JString,JString,jint,jint,jboolean,), 
-        ATname,Bname,C,CT,rowfilter,colfilterAT,colfilterB,-1,-1,true)
+        ATname,Bname,C,CT,toDBstring(rowfilter),toDBstring(colfilterAT),toDBstring(colfilterB),-1,-1,true)
     
     if ~isempty(CT)
         return AT.DB[C,CT]
@@ -46,40 +46,6 @@ function tablemult(AT::DBtableType,B::DBtableType,C::AbstractString,CT::Abstract
 
 end
 
- # Helper function to convert Assoc fields to an Accumulo-friendly string
- function toDBstring(input)
-    StringArray = Array{T} where T <: AbstractString
-    NumericArray = Array{T} where T <: Number
-    UnionArray = Array{T} where T <: Union{AbstractString,Number}
-    
-    if isa(input,StringArray)
-        output = join(input,"\n")*"\n"
-    elseif isa(input,NumericArray)
-        output = join(string.(input),"\n")*"\n"
-    elseif isa(input,UnionArray)
-        output = join(convert(Array{AbstractString},input),"\n")*"\n"
-    elseif isa(input,Colon)
-        output = ":"
-    elseif isa(input,StartsWith)
-        str = input.inputString
-        output = ""
-        del = str[end]
-        idx1 = 1
-        idx2 = search(str,del,idx1+1)
+ 
 
-        while idx2 > 0
-            output = output*str[idx1:idx2]*":"*del*str[idx1:idx2-1]*Char(127)*del
-            idx1 = idx2+1
-            idx2 = search(str,del,idx2+1)
-        end
-    else
-        output = input
-    end
-    
-    return output
-end
-
-UnionArray = Array{Union{AbstractString,Number}}
-ValidQueryTypes = Union{Colon,AbstractString,Array,UnionArray,StartsWith}
-
-tablemult(AT::DBtableType,B::DBtableType,C::AbstractString,CT::AbstractString=""; rowfilter::ValidQueryTypes="",colfilterAT::ValidQueryTypes="",colfilterB::ValidQueryTypes="") = tablemult(AT,B,C,CT; rowfilter=toDBstring(rowfilter),colfilterAT=toDBstring(colfilterAT),colfilterB=toDBstring(colfilterB))
+#tablemult(AT::DBtableType,B::DBtableType,C::AbstractString,CT::AbstractString=""; rowfilter::ValidQueryTypes=[],colfilterAT::ValidQueryTypes=[],colfilterB::ValidQueryTypes=[]) = tablemult(AT,B,C,CT; rowfilter=toDBstring(rowfilter),colfilterAT=toDBstring(colfilterAT),colfilterB=toDBstring(colfilterB))
