ce5a94ba46302122920dde9e7600c686b2ac7649
Lauren Milechin <lauren.milechin@mit.edu>
2018-10-17 09:45:59 -0400

    fix isempty check

--- src/ReadCSV.jl
+++ src/ReadCSV.jl
@@ -1,13 +1,13 @@
 function ReadCSV(fname)
     inDim = readdlm(fname,',')
-    (rowN,colN) = size(inDim)
+    rowN,colN = size(inDim)
     row = [];
     col = [];
     val = [];
     if colN >=2
         for x = 2:colN
             for y = 2:rowN
-                if ~isempty(inDim[x,y])
+                if ~isempty(inDim[y,x])
                     push!(row, inDim[y,1])
                     push!(col, inDim[1,x])
                     push!(val, inDim[y,x])
