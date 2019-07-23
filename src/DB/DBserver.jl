e356914636029747049de0b2095c2dee4042f9d3
Nathan <nkim8@mit.edu>
2019-07-22 22:14:41 -0400

    edgebfs not fixed bc i deleted stuff oopsssss

--- src/DB/DBserver.jl
+++ src/DB/DBserver.jl
@@ -27,6 +27,7 @@ function dbinit()
     if ~JavaCall.isloaded()
         JavaCall.addClassPath(joinpath(dirname(pathof(D4M)),"..","libext","*"))
         JavaCall.addClassPath(joinpath(dirname(pathof(D4M)),"..","lib","graphulo-3.0.0.jar"))
+        JavaCall.addClassPath(joinpath(dirname(pathof(D4M)),"..","lib","graphulo-3.0.0.jar"))
         JavaCall.init()
     else
         println("JVM already initialized")
