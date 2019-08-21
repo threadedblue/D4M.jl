9dd3a567243db7cdaf1fc11bd2e0f7e3c8354a43
Nathan <nkim8@mit.edu>
2019-08-21 09:03:12 -0400

    adj ktruss

--- src/DB/ktruss.jl
+++ src/DB/ktruss.jl
@@ -1,6 +1,6 @@
 using JavaCall
 
-function ktruss()
+function ktrussadj(A::DBtableType, Rtable::AbstractString, k::Number, filterRowCol::AbstractString, forceDelete::Bool; maxiter = 2^31 - 1::Number)
 # /**
 # * From input <b>unweighted, undirected</b> adjacency table Aorig, put the k-Truss
 # * of Aorig in Rfinal.
@@ -24,5 +24,41 @@ function ktruss()
 #                      String filterRowCol, boolean forceDelete,
 #                      Authorizations Aauthorizations, String RNewVisibility,
 #                      int maxiter) {
+
+    if isa(A, DBtablePair)
+        Atable = A.name1
+    else
+        Atable = A.name
+    end
+
+    JAuthorizations = @jimport "org.apache.accumulo.core.security.Authorizations"
+    emptyAuth = JAuthorizations((), )
+    newVisibility = ""
+
+    result = jcall(DB.Graphulo,"kTrussAdj", jlong, 
+        (JString, JString, jint, JString, jboolean, JAuthorizations, JString, jint), 
+        Atable, Rtable, k, toDBstring(filterRowCol), forceDelete, emptyAuth, newVisibility, maxiter)
+
+end
+
+function ktrussedge()
+    # /**
+    # * From input <b>unweighted, undirected</b> incidence table Eorig, put the k-Truss
+    # * of Eorig in Rfinal.  Needs transpose ETorig, and can output transpose of k-Truss subgraph too.
+    # * @param Eorig Unweighted, undirected incidence table.
+    # * @param ETorig Transpose of input incidence table.
+    # *               Optional.  Created on the fly if not given.
+    # * @param Rfinal Does not have to previously exist. Writes the kTruss into Rfinal if it already exists.
+    # *               Use a combiner if you want to sum it in.
+    # * @param RTfinal Does not have to previously exist. Writes in the transpose of the kTruss subgraph.
+    # * @param k Trivial if k <= 2.
+    # * @param edgeFilter Filter on rows of Eorig, i.e., the edges in an incidence table.
+    # * @param forceDelete False means throws exception if the temporary tables used inside the algorithm already exist.
+    # *                    True means delete them if they exist.
+    # * @param Eauthorizations Authorizations for scanning Atable. Null means use default: Authorizations.EMPTY
+    # * @return  nnz of the kTruss subgraph, which is 2* the number of edges in the kTruss subgraph.
+    # *          Returns -1 if k < 2 since there is no point in counting the number of edges.
+    # *  public long kTrussEdge(String Eorig, String ETorig, String Rfinal, String RTfinal, int k,
+    # String edgeFilter, boolean forceDelete, Authorizations Eauthorizations)
 
 end
