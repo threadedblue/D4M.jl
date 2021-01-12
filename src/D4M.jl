469e2d1df52fefe7326f1f7608b932484e53a3b6
Geoffry Roberts <roberts_geoffry.@bah.com>
2021-01-12 15:35:55 -0500

    Synch

--- src/D4M.jl
+++ src/D4M.jl
@@ -11,7 +11,7 @@ module D4M
     import PyPlot: spy
     import JLD: writeas, readas
 
-    export  Assoc, DBtablePair,
+    export  Assoc, DBtable, DBtablePair,
             StartsWith,
             CatKeyMul, CatValMul,
             CatStr, SplitStr, NumStr,
