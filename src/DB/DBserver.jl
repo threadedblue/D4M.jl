e0d80f5dc01f448c1c304d9063d84671a975ec48
Geoffry Roberts <roberts_geoffry.@bah.com>
2021-03-02 13:20:25 -0500

    Remove Graphulo10

--- src/DB/DBserver.jl
+++ src/DB/DBserver.jl
@@ -27,7 +27,7 @@ function dbinit()
     println("D4MDir=" * joinpath(dirname(pathof(D4M)),"..","libext"))
     if ~JavaCall.isloaded()
         JavaCall.addClassPath(joinpath(dirname(pathof(D4M)),"..","libext","*"))
-        JavaCall.addClassPath(joinpath(dirname(pathof(D4M)),"..","lib","graphulo-3.0.0.jar"))
+        JavaCall.addClassPath(joinpath(dirname(pathof(D4M)),"..","lib","graphulo-3.2.0.jar"))
         JavaCall.init()
     else
         println("JVM already initialized")
